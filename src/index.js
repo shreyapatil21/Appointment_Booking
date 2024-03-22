c5a66fcb2937444611123fc32839b9407da8e3c1
Samnit Patil <samnitpatil9882@gmail.com>
2024-03-22 10:54:14 +0530

    cors successful setup

--- src/index.js
+++ src/index.js
@@ -7,35 +7,30 @@ dotenv.config({ path: "./env" });
 // Databasr connection
 connectDB();
 
-import paitentRoutes from "./routs/patientRoutes.js"
-import doctorRoutes from "./routs/doctorRoutes.js"
+import paitentRoutes from "./routs/patientRoutes.js";
+import doctorRoutes from "./routs/doctorRoutes.js";
 import appointmentRoutes from "./routs/appointmentRoutes.js";
-import cookieParser from 'cookie-parser';
-import express from 'express';
+import cookieParser from "cookie-parser";
+import express from "express";
 const app = express();
 
 app.use(bodyParser.json()); // Parse JSON bodies
 app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static('public'));
-
-//Use routes
-// app.use(cors());
+app.use(express.static("public"));
 app.use(
-    cors({
-        credentials: true,
-        methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-        origin: '*',
-allowedHeaders: [
-    'Content-Type',
-  ],    
-    })
+  cors({
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: true,
+    allowedHeaders: ["Content-Type"],
+  }),
 );
 
 app.use("/patient", paitentRoutes);
 app.use("/doctor", doctorRoutes);
-app.use("/appointment",appointmentRoutes);
+app.use("/appointment", appointmentRoutes);
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
